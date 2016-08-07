f0a930e58697021c501b547c100ea15444f25b4e
21hook <2765049749@qq.com>
2016-08-07 12:17:20 +0800

    Programming Assignment 1

--- Assignment 1 (Simple C++)/perfectnum.cpp
+++ Assignment 1 (Simple C++)/perfectnum.cpp
@@ -1,10 +1,10 @@
 /*
- * Program: perfectnum.cpp
- *
- * Program used to generate the perfect number.
- * i.e 6 = 1+2+3, 28 = 1+2+4+7+14
- * Nmubers N can be divided as divisor less than N itself.
- */
+ Program: perfectnum.cpp
+ 
+ Program used to generate the perfect number.
+ i.e 6 = 1+2+3, 28 = 1+2+4+7+14
+ Nmubers N can be divided as divisor less than N itself.
+*/
 
 #include <iostream>
 #include "console.h" // for console
@@ -15,11 +15,11 @@ using namespace std;
 
 
 /*
- * bool = IsPerfect(int n);
- *
- * Return true If the integer is perfect numbers.
- * Return false if not.
- */
+ bool = IsPerfect(int n);
+ 
+ Return true If the integer is perfect numbers.
+ Return false if not.
+*/
 bool IsPerfect(int n) {
     int x, sum;
 
